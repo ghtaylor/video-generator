840c7ab76fdd34e6f12c42b141d5252c2e0336a7
George Taylor <george.taylor@cazoo.co.uk>
2023-10-07 13:43:35 +0100

    feat: implement speech mark and audio buffer retrieval from polly with integration tests

---
+++ __tests__/integration/adapters/pollySpeechService.test.ts
@@ -0,0 +1,166 @@
+import { PollyClient, SynthesizeSpeechCommandOutput } from "@aws-sdk/client-polly";
+import { ValidationError } from "@core/errors/ValidationError";
+import { PollySpeechService } from "@infrastructure/adapters/pollySpeechService";
+import { mock, mockDeep } from "jest-mock-extended";
+import { err, ok } from "neverthrow";
+import type { PartialDeep } from "type-fest";
+
+const pollyClient = mockDeep<PollyClient>();
+
+const pollySpeechService = new PollySpeechService(pollyClient);
+
+describe("PollySpeechService - Integration Tests", () => {
+  describe("GIVEN the PollyClient responds for an MP3 request", () => {
+    describe("AND the AudioStream in the response is valid", () => {
+      const response: PartialDeep<SynthesizeSpeechCommandOutput> = {
+        ContentType: "audio/mpeg",
+        AudioStream: {
+          transformToByteArray: async () => new Uint8Array([1, 2, 3]),
+        },
+      };
+
+      beforeEach(() => {
+        pollyClient.send.mockResolvedValue(response as never);
+      });
+
+      describe("WHEN `getSpeechAudio` is called", () => {
+        const text = "Hello world";
+
+        test("THEN it should return the audio", async () => {
+          await expect(pollySpeechService.getSpeechAudio(text)).resolves.toEqual(ok(Buffer.from([1, 2, 3])));
+        });
+      });
+    });
+
+    describe("AND the AudioStream in the response is undefined", () => {
+      const response: PartialDeep<SynthesizeSpeechCommandOutput> = {
+        ContentType: "audio/mpeg",
+      };
+
+      beforeEach(() => {
+        pollyClient.send.mockResolvedValue(response as never);
+      });
+
+      describe("WHEN `getSpeechAudio` is called", () => {
+        const text = "Hello world";
+
+        test("THEN it should return a ValidationError", async () => {
+          await expect(pollySpeechService.getSpeechAudio(text)).resolves.toEqual(
+            err(new ValidationError("Audio stream is empty")),
+          );
+        });
+      });
+    });
+
+    describe("AND the AudioStream throws an Error when transforming to a byte array", () => {
+      const response: PartialDeep<SynthesizeSpeechCommandOutput> = {
+        ContentType: "audio/mpeg",
+        AudioStream: {
+          transformToByteArray: async () => {
+            throw new Error("Transform error");
+          },
+        },
+      };
+
+      beforeEach(() => {
+        pollyClient.send.mockResolvedValue(response as never);
+      });
+
+      describe("WHEN `getSpeechAudio` is called", () => {
+        const text = "Hello world";
+
+        test("THEN it should return a ValidationError", async () => {
+          await expect(pollySpeechService.getSpeechAudio(text)).resolves.toEqual(
+            err(new ValidationError("Audio stream is invalid", new Error("Transform error"))),
+          );
+        });
+      });
+    });
+  });
+
+  describe("GIVEN the PollyClient responds for a JSON (Speech Marks) request", () => {
+    describe("AND the AudioStream in the response is valid", () => {
+      const audioStreamByteArray = new TextEncoder().encode(
+        `{"time":6,"type":"word","start":0,"end":5,"value":"Hello"}
+            {"time":732,"type":"word","start":7,"end":12,"value":"world"}`,
+      );
+
+      const response: PartialDeep<SynthesizeSpeechCommandOutput> = {
+        ContentType: "application/x-json-stream",
+        AudioStream: {
+          transformToByteArray: async () => audioStreamByteArray,
+        },
+      };
+
+      beforeEach(() => {
+        pollyClient.send.mockResolvedValue(response as never);
+      });
+
+      describe("WHEN `getSpeechMarks` is called", () => {
+        const text = "Hello world";
+
+        test("THEN it should return the SpeechMarks", async () => {
+          await expect(pollySpeechService.getSpeechMarks(text)).resolves.toEqual(
+            ok([
+              {
+                start: 0,
+                end: 5,
+                value: "Hello",
+              },
+              {
+                start: 7,
+                end: 12,
+                value: "world",
+              },
+            ]),
+          );
+        });
+      });
+    });
+
+    describe("AND the AudioStream in the response is undefined", () => {
+      const response: PartialDeep<SynthesizeSpeechCommandOutput> = {
+        ContentType: "application/x-json-stream",
+      };
+
+      beforeEach(() => {
+        pollyClient.send.mockResolvedValue(response as never);
+      });
+
+      describe("WHEN `getSpeechMarks` is called", () => {
+        const text = "Hello world";
+
+        test("THEN it should return a ValidationError", async () => {
+          await expect(pollySpeechService.getSpeechMarks(text)).resolves.toEqual(
+            err(new ValidationError("Audio stream is empty")),
+          );
+        });
+      });
+    });
+
+    describe("AND the AudioStream throws an Error when transforming to a byte array", () => {
+      const response: PartialDeep<SynthesizeSpeechCommandOutput> = {
+        ContentType: "application/x-json-stream",
+        AudioStream: {
+          transformToByteArray: async () => {
+            throw new Error("Transform error");
+          },
+        },
+      };
+
+      beforeEach(() => {
+        pollyClient.send.mockResolvedValue(response as never);
+      });
+
+      describe("WHEN `getSpeechMarks` is called", () => {
+        const text = "Hello world";
+
+        test("THEN it should return a ValidationError", async () => {
+          await expect(pollySpeechService.getSpeechMarks(text)).resolves.toEqual(
+            err(new ValidationError("Audio stream is invalid", new Error("Transform error"))),
+          );
+        });
+      });
+    });
+  });
+});
--- src/infrastructure/adapters/pollySpeechService.ts
+++ src/infrastructure/adapters/pollySpeechService.ts
@@ -1,10 +1,11 @@
-import { PollyClient } from "@aws-sdk/client-polly";
+import { PollyClient, SynthesizeSpeechCommand, SynthesizeSpeechCommandOutput } from "@aws-sdk/client-polly";
 import { NetworkError } from "@core/errors/NetworkError";
 import { ParseError } from "@core/errors/ParseError";
 import { UnknownError } from "@core/errors/UnknownError";
+import { ValidationError } from "@core/errors/ValidationError";
 import { SpeechService } from "@core/speechService";
 import { Speech, SpeechMark } from "@domain/Speech";
-import { Result, ResultAsync, fromThrowable } from "neverthrow";
+import { Result, ResultAsync, err, errAsync, fromPromise, fromThrowable, ok } from "neverthrow";
 
 export class PollySpeechService implements SpeechService {
   constructor(private readonly pollyClient: PollyClient) {}
@@ -20,12 +21,49 @@ export class PollySpeechService implements SpeechService {
     )();
   }
 
-  getSpeechMarks(text: string): ResultAsync<string, NetworkError | UnknownError> {
-    throw new Error("Method not implemented.");
+  private sendPollyCommand(command: SynthesizeSpeechCommand): ResultAsync<SynthesizeSpeechCommandOutput, NetworkError> {
+    return fromPromise(
+      this.pollyClient.send(command),
+      (error) => new NetworkError("Polly API error", error instanceof Error ? error : undefined),
+    );
   }
 
-  getSpeechAudio(text: string): ResultAsync<Buffer, NetworkError | UnknownError> {
-    throw new Error("Method not implemented.");
+  private getBufferFromAudioStream(
+    audioStream?: SynthesizeSpeechCommandOutput["AudioStream"],
+  ): ResultAsync<Buffer, ValidationError> {
+    if (audioStream === undefined) return errAsync(new ValidationError("Audio stream is empty"));
+
+    return fromPromise(
+      audioStream.transformToByteArray(),
+      (error) => new ValidationError("Audio stream is invalid", error instanceof Error ? error : undefined),
+    ).map((byteArray) => Buffer.from(byteArray));
+  }
+
+  getSpeechMarks(text: string): ResultAsync<SpeechMark[], NetworkError | ValidationError> {
+    const command = new SynthesizeSpeechCommand({
+      OutputFormat: "json",
+      SpeechMarkTypes: ["word"],
+      Text: text,
+      VoiceId: "Joanna",
+    });
+
+    return this.sendPollyCommand(command)
+      .map((response) => response.AudioStream)
+      .andThen(this.getBufferFromAudioStream)
+      .map((audioBuffer) => audioBuffer.toString("utf-8"))
+      .andThen(this.parseSpeechMarks);
+  }
+
+  getSpeechAudio(text: string): ResultAsync<Buffer, NetworkError | ValidationError> {
+    const command = new SynthesizeSpeechCommand({
+      OutputFormat: "mp3",
+      Text: text,
+      VoiceId: "Joanna",
+    });
+
+    return this.sendPollyCommand(command)
+      .map((response) => response.AudioStream)
+      .andThen(this.getBufferFromAudioStream);
   }
 
   generateSpeech(text: string): ResultAsync<Speech, UnknownError | NetworkError> {
