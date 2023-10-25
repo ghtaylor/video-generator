f44d0ab4318f7753ff1ea6a056e15818b495c5ae
George Taylor <george.taylor@cazoo.co.uk>
2023-10-25 16:56:38 +0100

    fix: update video component to utilise new properties

--- src/core/video/Composition.tsx
+++ src/core/video/Composition.tsx
@@ -1,14 +1,14 @@
 import { VideoOptions } from "@domain/Video";
 import { AbsoluteFill, Audio, OffthreadVideo, Series, staticFile } from "remotion";
 
-export const MyComposition: React.FC<VideoOptions> = ({ sections, speechAudioLocation }) => {
+export const MyComposition: React.FC<VideoOptions> = ({ sections, speechAudioUrl }) => {
   return (
     <>
       <Series>
-        {sections.map(({ text, durationInFrames, backgroundVideoLocation }) => (
+        {sections.map(({ text, durationInFrames, backgroundVideoUrl }) => (
           <Series.Sequence key={text} durationInFrames={durationInFrames} className="items-center justify-center">
             <AbsoluteFill className="-z-50">
-              <OffthreadVideo src={backgroundVideoLocation} />
+              <OffthreadVideo src={backgroundVideoUrl} />
             </AbsoluteFill>
 
             <h1 className="text-7xl tracking-wider font-bold text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
@@ -17,7 +17,7 @@ export const MyComposition: React.FC<VideoOptions> = ({ sections, speechAudioLoc
           </Series.Sequence>
         ))}
       </Series>
-      <Audio src={speechAudioLocation} />
+      <Audio src={speechAudioUrl} />
     </>
   );
 };
