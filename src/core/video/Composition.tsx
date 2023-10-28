3627c18f8859302fece9abeb29b9a7de46fe64a4
George Taylor <george.taylor@cazoo.co.uk>
2023-10-28 12:59:08 +0100

    feat: new font and styling for video

--- src/core/video/Composition.tsx
+++ src/core/video/Composition.tsx
@@ -1,5 +1,8 @@
 import { VideoOptions } from "@domain/Video";
+import { loadFont } from "@remotion/google-fonts/Playfair";
 import { AbsoluteFill, Audio, OffthreadVideo, Series } from "remotion";
+
+const { fontFamily } = loadFont();
 
 export const MyComposition: React.FC<VideoOptions> = ({ sections, speechAudioUrl }) => {
   return (
@@ -7,11 +10,17 @@ export const MyComposition: React.FC<VideoOptions> = ({ sections, speechAudioUrl
       <Series>
         {sections.map(({ text, durationInFrames, backgroundVideoUrl }) => (
           <Series.Sequence key={text} durationInFrames={durationInFrames} className="items-center justify-center">
-            <AbsoluteFill className="-z-50">
-              <OffthreadVideo src={backgroundVideoUrl} />
+            <AbsoluteFill className="-z-50 bg-black">
+              <OffthreadVideo
+                src={backgroundVideoUrl}
+                className="object-cover relative opacity-50 grayscale contrast-150"
+              />
             </AbsoluteFill>
 
-            <h1 className="text-7xl tracking-wider font-bold text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+            <h1
+              className="text-5xl font-light text-white leading-tight text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] p-24"
+              style={{ fontFamily }}
+            >
               {text}
             </h1>
           </Series.Sequence>
