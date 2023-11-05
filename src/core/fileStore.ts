e55698acfe43676c7157bd83c8b09a4df8eace20
George Taylor <georgetaylor@Georges-MacBook-Pro.local>
2023-11-05 23:11:46 +0000

    feat: implement get file buffer from S3

--- src/core/fileStore.ts
+++ src/core/fileStore.ts
@@ -8,4 +8,5 @@ export interface FileStore {
   store(path: FileLocation, buffer: Buffer): ResultAsync<FileLocation, NetworkError>;
   listFiles(path: FileLocation): ResultAsync<FileLocation[], NetworkError>;
   getUrl(path: FileLocation): ResultAsync<FileUrl, NetworkError>;
+  getBuffer(path: FileLocation): ResultAsync<Buffer, NetworkError>;
 }
--- src/infrastructure/adapters/s3FileStore.ts
+++ src/infrastructure/adapters/s3FileStore.ts
@@ -2,7 +2,7 @@ import { GetObjectCommand, ListObjectsV2Command, PutObjectCommand, S3Client } fr
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { NetworkError } from "@core/errors/NetworkError";
 import { FileLocation, FileStore, FileUrl } from "@core/fileStore";
-import { ResultAsync, fromPromise } from "neverthrow";
+import { ResultAsync, errAsync, fromPromise } from "neverthrow";
 
 export class S3FileStore implements FileStore {
   constructor(
@@ -53,5 +53,26 @@ export class S3FileStore implements FileStore {
       }),
       (error) => new NetworkError("Failed to get file url", error instanceof Error ? error : undefined),
     );
+  }
+
+  getBuffer(path: string): ResultAsync<Buffer, NetworkError> {
+    const command = new GetObjectCommand({
+      Bucket: this.bucketName,
+      Key: path,
+    });
+
+    return fromPromise(
+      this.s3Client.send(command),
+      (error) => new NetworkError("Failed to get file", error instanceof Error ? error : undefined),
+    )
+      .andThen((response) => {
+        if (response.Body === undefined) return errAsync(new NetworkError("Failed to get file"));
+
+        return fromPromise(
+          response.Body.transformToByteArray(),
+          (err) => new NetworkError("Failed to transform file to byte array", err instanceof Error ? err : undefined),
+        );
+      })
+      .map(Buffer.from);
   }
 }
