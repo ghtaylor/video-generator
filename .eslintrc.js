9e076db94653bcce49c9bec6986a93293a998d5a
George Taylor <georgetaylor@Georges-MacBook-Pro.local>
2023-11-05 23:09:34 +0000

    fix: eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,8 @@
 /* eslint-env node */
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   root: true,
-  overrides: {
-    rules: {
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
-    },
-  },
 };
+
