e560635fdfe2036938b4442d6ef52425546c76f7
George Taylor <ghtaylor@outlook.com>
2023-11-10 18:54:58 +0000

    feat: implement pino logger to replace winston

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -1,4 +1,11 @@
+import { Result } from "neverthrow";
+
 export interface Logger {
-  info(message: string): this;
-  error(error: Error): this;
+  info(message: string): void;
+  info(message: string, data?: unknown): void;
+
+  error(message: string): void;
+  error(message: string, error?: Error): void;
+
+  logResult<T, E extends Error>(result: Result<T, E>, message?: string): void;
 }
---
+++ src/infrastructure/adapters/pinoLogger.ts
@@ -0,0 +1,32 @@
+import { Logger } from "@core/logger";
+import { Result } from "neverthrow";
+import pino, { Logger as Pino } from "pino";
+
+export class PinoLogger implements Logger {
+  constructor(private logger: Pino) {}
+  logResult<T, E extends Error>(result: Result<T, E>, message?: string): void {
+    if (result.isOk()) {
+      this.info(message ?? "OK result received", { result: result.value });
+      return;
+    }
+
+    this.error(message ?? "Error result received", result.error);
+  }
+
+  static build(): PinoLogger {
+    return new PinoLogger(pino());
+  }
+
+  info(message: string, data?: unknown): void {
+    this.logger.info(data, message);
+  }
+
+  error(message: string, error?: Error): void {
+    if (error instanceof Error) {
+      this.logger.error(error, message);
+      return;
+    }
+
+    this.logger.error(message);
+  }
+}
