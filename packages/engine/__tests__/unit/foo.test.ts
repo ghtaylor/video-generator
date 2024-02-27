6f544a1557325be2b7db5e5a229eee7f29a6ef6d
George Taylor <ghtaylor@outlook.com>
2024-02-27 17:22:09 +0700

    chore: remove redundant test file

--- packages/engine/__tests__/unit/foo.test.ts
+++
@@ -1,6 +0,0 @@
-import { Quote } from "@video-generator/domain/Quote";
-import zodToJsonSchema from "zod-to-json-schema";
-
-test("foo", () => {
-  console.log(JSON.stringify(zodToJsonSchema(Quote)));
-});
