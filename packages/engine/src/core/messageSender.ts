d4a8c43ffa363ed727d16139c9cf3f0f7d6d7a39
George Taylor <ghtaylor@outlook.com>
2024-02-27 17:30:05 +0700

    chore: remove redundant implementations and update test file dependencies

--- packages/engine/src/core/messageSender.ts
+++
@@ -1,6 +0,0 @@
-import { ResultAsync } from "neverthrow";
-import { ServiceError } from "@core/errors/ServiceError";
-
-export interface MessageSender<TMessage> {
-  send(message: TMessage): ResultAsync<TMessage, ServiceError>;
-}
